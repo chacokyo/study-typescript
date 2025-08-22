2281512328759012d4a3cf1129a3cb4261819a32
chacokyo <3jisoo5@naver.com>
2025-08-22 23:43:29 +0900

    [add] 객체 타입
    - type
    - interface
    - 객체 리터럴
    - 인덱스 시그니처 [key: type] : any

--- 2-basic-types/2.3-typing-objects/2.3.2-exercise/exercise.ts
+++ 2-basic-types/2.3-typing-objects/2.3.2-exercise/exercise.ts
@@ -6,10 +6,14 @@
 // - title: string
 // - author: string
 // - pages: number
-
+const book = {
+  title: 'hamster',
+  author: 'jisu',
+  pages: 1004,
+}
 
 // 질문 2: book 객체 리터럴에 다른 속성을 추가하려고 하면 TypeScript가 허용할까요?
-
+book.hamster = 'no'
 
 // 질문 3: 다음과 같은 속성을 가진 `Car` 인터페이스를 정의하세요.
 // - make: string
@@ -17,10 +21,39 @@
 // - year: number
 // 그런 다음 이 인터페이스를 따르는 객체를 만드세요.
 
+interface Car {
+  make: string
+  model: string
+  year: number
+}
+
+const hamster: Car = { make: 'hamster', model: 'frog', year: 2025 }
 
 // 질문 4: `Car`와 동일한 형태를 가진 타입 별칭 `Car2`를 정의하세요.
 // 그리고 이 타입 별칭을 따르는 객체를 만드세요.
 
+type Car2 = {
+  make: string
+  model: string
+  year: number
+}
+
+const ham: Car2 = { make: 'Toyota', model: 'mola', year: 1004 }
 
 // 질문 5: `Car`와 동일한 형태를 가지면서,
 // 추가적인 어떤 프로퍼티도 허용하는 `Car3` 인터페이스를 만드세요.
+type Car3 = {
+  make: string
+  model: string
+  year: number
+  [key: string]: any
+}
+
+const jisu: Car3 = {
+  make: 'tokyo',
+  model: 'tokyo',
+  year: 1004,
+  name: 'hamster',
+}
+
+console.log(jisu)
