954cca0f0abc51e39ec2a83369a0275a42a81f1a
chacokyo <3jisoo5@naver.com>
2025-08-22 23:06:14 +0900

    [add] 배열 타입, 튜플 타입

--- 2-basic-types/2.2-typing-arrays-and-tuples/2.2.1-examples/example-1.ts
+++ 2-basic-types/2.2-typing-arrays-and-tuples/2.2.1-examples/example-1.ts
@@ -5,10 +5,10 @@ const numbers: number[] = [1, 2, 3]
 const numbers2: Array<number> = [1, 2, 3]
 //    ^?
 
-// 튜플(Tuple) 타입
+// 튜플(Tuple) 타입 (정해진 순서대로)
 const tuple: [number, string] = [1, '타입스크립트']
 
-// 제네릭 타입을 사용한 혼합 배열 
+// 제네릭 타입을 사용한 혼합 배열
 // - 위의 튜플과 유사하지만, 완전히 동일하지 않음.
 const tuple2: Array<number | string> = [1, '타입스크립트']
 //    ^?
