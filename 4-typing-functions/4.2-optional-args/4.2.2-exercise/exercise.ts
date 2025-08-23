5d56c88193b6de5ab3d559347fefe9de8b43496a
chacokyo <3jisoo5@naver.com>
2025-08-23 22:57:17 +0900

    [add] 옵셔널 매개변수
    - 매개변수 이름 뒤에 ? 를 붙여 선택적으로 만들 수 있다.

--- 4-typing-functions/4.2-optional-args/4.2.2-exercise/exercise.ts
+++ 4-typing-functions/4.2-optional-args/4.2.2-exercise/exercise.ts
@@ -3,11 +3,22 @@
 // --------------------------------------------------------------------------
 
 // 1. 두 번째 매개변수를 선택적으로 만듭니다.
-function concatStrings(str1: string, str2: string): string {
+function concatStrings(str1: string, str2?: string): string {
   return str1 + (str2 ?? '')
 }
 
 // 2. 두 번째 매개변수의 기본값을 빈 문자열로 지정합니다.
-function concatStrings2(str1: string, str2: string): string {
+function concatStrings2(str1: string, str2: string | undefined = ''): string {
   return str1 + str2
 }
+
+function hamster(name: string, comment?: string) {
+  if (comment) {
+    return `${comment}, ${name}`
+  } else {
+    return `${name}`
+  }
+}
+
+console.log(hamster('jisu', 'hello'))
+console.log(hamster('toki'))
