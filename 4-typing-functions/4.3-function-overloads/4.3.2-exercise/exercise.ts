bb817d6c4dbb2ca9cb480488633552fba0748674
chacokyo <3jisoo5@naver.com>
2025-08-23 23:03:56 +0900

    [add] 함수 오버로드
    - 동일한 함수 이름에 여러개의 함수 시그니처 정의.
    - 전달되는 인자에 따라 다양한 방식으로 함수 호출 가능.

--- 4-typing-functions/4.3-function-overloads/4.3.2-exercise/exercise.ts
+++ 4-typing-functions/4.3-function-overloads/4.3.2-exercise/exercise.ts
@@ -1,6 +1,12 @@
 // Exercise 1: Create function overloads for a function named `getLength`
 // that can take either a string or an array and return their length.
+function getLength(type: string): number
+function getLength(type: object): number
+
+function getLength(type: any) {
+  return type.length
+}
 
 // Test your implementation:
-// getLength("TypeScript"); // 10
-// getLength([1, 2, 3, 4, 5]); // 5
+console.log(getLength('TypeScript'))
+console.log(getLength([1, 2, 3, 4, 5]))
