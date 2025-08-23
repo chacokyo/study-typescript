c779bedcd9ccd97d0a00cd82d2714e82498f1eb8
chacokyo <3jisoo5@naver.com>
2025-08-24 01:50:37 +0900

    [add] 네임스페이스
    - 전역 스코프에서 작업할 때 네임스페이스로 관련 코드를 관리하고 캡슐화할 수 있음.

--- 5-modules/5.6-namespaces/5.6.2-exercise/exercise.ts
+++ 5-modules/5.6-namespaces/5.6.2-exercise/exercise.ts
@@ -4,11 +4,22 @@
 
 // 문제 1
 // 두 숫자를 받아 합을 반환하는 `add` 함수를 포함하는 `MathOperations` 네임스페이스를 만드세요.
-
+namespace MathOperations {
+  export function add(a: number, b: number): number {
+    return a + b
+  }
+}
 
 // 문제 2
 // 두 숫자를 받아 차를 반환하는 `subtract` 함수를 `MathOperations` 네임스페이스에 추가하세요.
 
-
+namespace MathOperations {
+  export function subtract(a: number, b: number): number {
+    return a - b
+  }
+}
 // 문제 3
 // `MathOperations` 네임스페이스를 사용하여 두 숫자를 더하고 빼서, 결과를 콘솔에 출력하세요.
+
+console.log(MathOperations.add(1, 2))
+console.log(MathOperations.subtract(1, 2))
