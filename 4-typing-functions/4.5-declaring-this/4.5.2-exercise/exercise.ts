97310ba642efc7eb0847f445cc9d71a57a6edf42
chacokyo <3jisoo5@naver.com>
2025-08-23 23:39:10 +0900

    [add] 함수에서의 this 타입
    - 일반 함수에서도 this 컨텍스트를 명시할 수 있다.
    - 함수의 매개변수 선언 위치에 this를 사용하여 this의 타입을 지정할 수 있다.

--- 4-typing-functions/4.5-declaring-this/4.5.2-exercise/exercise.ts
+++ 4-typing-functions/4.5-declaring-this/4.5.2-exercise/exercise.ts
@@ -5,12 +5,29 @@
 // 문제 1
 // `user`라는 객체 리터럴을 만들고, `username` 속성과 `introduce`라는 메서드를 추가하세요.
 // `introduce` 메서드는 `this`를 사용하여 `username` 속성을 포함한 자기소개 메시지를 반환해야 합니다.
+const user = {
+  username: '지수',
+  introduce() {
+    return `${this.username}는 햄스터 입니다.`
+  },
+}
 
+console.log(user.introduce())
 
 // 문제 2
 // `Counter`라는 클래스를 만들고, `count` 속성을 0으로 초기화하세요.
 // 그리고 `increment`라는 메서드를 추가하여, `this`를 사용해 `count` 값을 1 증가시키세요.
+class Counter {
+  count: number = 0
+  increment() {
+    this.count += 1
+  }
+}
 
+const count = new Counter()
+console.log(count.count)
+count.increment()
+console.log(count.count)
 
 // 문제 3
 // 아래의 `person` 객체를 위해 인사 메시지를 출력하는 `sayHello` 함수를 만드세요.
