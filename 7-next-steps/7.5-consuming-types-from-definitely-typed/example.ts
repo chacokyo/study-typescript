aaac8f4d4707bb0ec6668c35db0354f187b07cec
chacokyo <3jisoo5@naver.com>
2025-08-24 12:02:31 +0900

    [add] 타입 정의가 없는 라이브러리
    - 타입 정의 패키지 설치
    - DefinitelyTyped 저장소에 가면 사용 가능한 타입 정의 볼 수 있음

--- 7-next-steps/7.5-consuming-types-from-definitely-typed/example.ts
+++ 7-next-steps/7.5-consuming-types-from-definitely-typed/example.ts
@@ -1,8 +1,17 @@
 // DefinitelyTyped에 별도의 타입이 있는 NPM 패키지 사용하기
 // pluraize 라이브러리는 전달된 영단어를 복수형으로 만들어 줍니다.
-import { plural } from 'pluralize'
+// bun 환경
+// import { plural } from 'pluralize'
 
-const pluraization = plural('library')
-//    ^?
+// const pluraization = plural('Library')
+// //    ^?
 
-console.log(pluraization) // 출력: "libraries"
+// console.log(pluraization) // 출력: "libraries"
+
+// node 환경
+import pluralize from 'pluralize'
+
+const { singular } = pluralize
+
+const singularization = singular('hamsters')
+console.log(singularization)
